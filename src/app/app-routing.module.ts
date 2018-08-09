ad7006f7e9dfc7a33a06aa5fe392715f2caae511
geraldynFrancioni <geraldyn.francioni@gmail.com>
2018-08-08 23:37:26 -0500

    Colocamos Navbar y rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import {AdminUserComponent} from './pages/admin-user/admin-user.component';
+import {HomeComponent} from './pages/home/home.component';
+import {LoginComponent} from './pages/login/login.component';
+import {NoFoundComponent} from './pages/no-found/no-found.component';
+import {RegisterComponent} from './pages/register/register.component';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'adminUser', component: AdminUserComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: '**', component: NoFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
