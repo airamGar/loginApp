776e45f5fd394b1f2e81dd365707a77f18e57816
geraldynFrancioni <geraldyn.francioni@gmail.com>
2018-08-08 22:58:18 -0500

    instalados componentes login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NavBarComponent } from './component/nav-bar/nav-bar.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
+import { AdminUserComponent } from './pages/admin-user/admin-user.component';
+import { NoFoundComponent } from './pages/no-found/no-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavBarComponent,
+    RegisterComponent,
+    LoginComponent,
+    AdminUserComponent,
+    NoFoundComponent
   ],
   imports: [
     BrowserModule,
