628744f7db672fbe625cd7ba7cac563b1073d5fd
geraldynFrancioni <geraldyn.francioni@gmail.com>
2018-08-09 00:39:26 -0500

    firebase y configuracion de environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAyd7wk3LBoGyRB2PoJK42O0pWx1wZQbl4",
+    authDomain: "loginapp-9b579.firebaseapp.com",
+    databaseURL: "https://loginapp-9b579.firebaseio.com",
+    projectId: "loginapp-9b579",
+    storageBucket: "",
+    messagingSenderId: "387175916898"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAyd7wk3LBoGyRB2PoJK42O0pWx1wZQbl4",
+    authDomain: "loginapp-9b579.firebaseapp.com",
+    databaseURL: "https://loginapp-9b579.firebaseio.com",
+    projectId: "loginapp-9b579",
+    storageBucket: "",
+    messagingSenderId: "387175916898"
+  }
 };
 
 /*
