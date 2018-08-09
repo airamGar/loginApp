83690d7c39881bc14ac231dbf2415dcab671a490
geraldynFrancioni <geraldyn.francioni@gmail.com>
2018-08-09 15:40:25 -0500

    habilitado registro de usuario por correo electronico

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +10,14 @@ import { RegisterComponent } from './pages/register/register.component';
 import { LoginComponent } from './pages/login/login.component';
 import { AdminUserComponent } from './pages/admin-user/admin-user.component';
 import { NoFoundComponent } from './pages/no-found/no-found.component';
+
+import { AngularFireModule } from '../../node_modules/angularfire2';
+import { AngularFireAuthModule } from '../../node_modules/angularfire2/auth';
+
+// import { AngularFireModule } from 'angularFire2';
+// import { AngularFireAuthModule } from 'angularFire2/auth';
+
+import { environment } from '../environments/environment';
 
 import {AuthService} from './service/auth.service';
 
@@ -24,7 +33,10 @@ import {AuthService} from './service/auth.service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig)
   ],
   providers: [AuthService],
   bootstrap: [AppComponent]
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '../../../../node_modules/@angular/router';
+import { AuthService } from '../../service/auth.service';
+// import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  public email: string;
+  public password: string;
 
-  constructor() { }
+  constructor(
+    public authService: AuthService,
+    public router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  onSubmitAddUser() {
+    this.authService.registerUser(this.email, this.password).then((res) => {
+      this.router.navigate(['/adminUser']);
+  }).catch((err) => {
+    console.log(err);
+  });
   }
 
 }
